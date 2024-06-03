2079783b85ea295edf84c57c785cc8f89b5f3123
Josh Mischung <josh@knoasis.io>
2024-06-02 17:49:37 -0700

    On branch main Your branch is up to date with 'origin/main'.
    chore: update test_app.py to reflect change from your_homepage.py to homepage.py
    
    Changes to be committed:
    	modified:   requirements.txt
    	modified:   tests/test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -9,7 +9,7 @@ def test_homepage():
     """
     Test the homepage functionality of the app.
     """
-    at = AppTest.from_file("../your_homepage.py").run()
+    at = AppTest.from_file("../homepage.py").run()
     assert not at.exception  # nosec B101
     assert at.title[0].value == "Welcome to Your App"  # nosec B101
     assert "This is the home page" in at.markdown[0].value  # nosec B101
@@ -19,7 +19,7 @@ def test_page_a():
     """
     Test the page A functionality of the app.
     """
-    at = AppTest.from_file("../your_homepage.py").run()
+    at = AppTest.from_file("../homepage.py").run()
     at.switch_page("pages/a_page.py").run()
     assert not at.exception  # nosec B101
     assert at.title[0].value == "Page A"  # nosec B101
@@ -30,7 +30,7 @@ def test_another_age():
     """
     Test the another page functionality of the app.
     """
-    at = AppTest.from_file("../your_homepage.py").run()
+    at = AppTest.from_file("../homepage.py").run()
     at.switch_page("pages/another_page.py").run()
     assert not at.exception  # nosec B101
     assert at.title[0].value == "Another Page"  # nosec B101
