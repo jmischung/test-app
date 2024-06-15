4eb03702ddf9bed5f34245cba55f7e21a3098c5c
Josh Mischung <josh@knoasis.io>
2024-06-15 08:10:53 -0700

    On branch develop Your branch is up to date with 'origin/develop'.
    chore: update test_app.py to reflect changes to file names
    
    Changes to be committed:
    	modified:   tests/test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -9,7 +9,7 @@ def test_homepage():
     """
     Test the homepage functionality of the app.
     """
-    at = AppTest.from_file("../homepage.py").run()
+    at = AppTest.from_file("../Homepage.py").run()
     assert not at.exception  # nosec B101
     assert at.title[0].value == "Welcome to Your App"  # nosec B101
     assert "This is the home page" in at.markdown[0].value  # nosec B101
@@ -19,8 +19,8 @@ def test_heroku_set_up_page():
     """
     Test the page A functionality of the app.
     """
-    at = AppTest.from_file("../homepage.py").run()
-    at.switch_page("pages/heroku_set_up.py").run()
+    at = AppTest.from_file("../Homepage.py").run()
+    at.switch_page("pages/2_Heroku_Set_Up.py").run()
     assert not at.exception  # nosec B101
     assert at.title[0].value == "Heroku Set Up"  # nosec B101
     assert "deploying this Streamlit app to Heroku" in at.markdown[0].value  # nosec B101
@@ -30,8 +30,8 @@ def test_github_config_page():
     """
     Test the another page functionality of the app.
     """
-    at = AppTest.from_file("../homepage.py").run()
-    at.switch_page("pages/github_configuration.py").run()
+    at = AppTest.from_file("../Homepage.py").run()
+    at.switch_page("pages/1_GitHub_Configuration.py").run()
     assert not at.exception  # nosec B101
     assert at.title[0].value == "GitHub Configuration"  # nosec B101
     assert "branch protection rules" in at.markdown[0].value  # nosec B101
