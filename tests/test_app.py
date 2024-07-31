bef2b8057c0e41a01392757901fba4e121c10bf6
Josh Mischung <josh@knoasis.io>
2024-07-31 13:45:36 -0700

    On branch develop Your branch is up to date with 'origin/develop'.
    test: modify directory string for Apptest
    
    Changes to be committed:
    	modified:   tests/test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -9,7 +9,7 @@ def test_homepage():
     """
     Test the homepage functionality of the app.
     """
-    at = AppTest.from_file("./Homepage.py").run()
+    at = AppTest.from_file("../Homepage.py").run()
     assert not at.exception  # nosec B101
     assert at.title[0].value == "Welcome to Your App"  # nosec B101
     assert "This is the home page" in at.markdown[0].value  # nosec B101
@@ -19,7 +19,7 @@ def test_heroku_set_up_page():
     """
     Test the page A functionality of the app.
     """
-    at = AppTest.from_file("./Homepage.py").run()
+    at = AppTest.from_file("../Homepage.py").run()
     at.switch_page("pages/2_Heroku_Set_Up.py").run()
     assert not at.exception  # nosec B101
     assert at.title[0].value == "Heroku Set Up"  # nosec B101
@@ -30,7 +30,7 @@ def test_github_config_page():
     """
     Test the another page functionality of the app.
     """
-    at = AppTest.from_file("./Homepage.py").run()
+    at = AppTest.from_file("../Homepage.py").run()
     at.switch_page("pages/1_GitHub_Configuration.py").run()
     assert not at.exception  # nosec B101
     assert at.title[0].value == "GitHub Configuration"  # nosec B101
